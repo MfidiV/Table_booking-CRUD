c8cfb7cc4d8e4207cfbf6f34dd4ea46994798e7c
Vuyolwethu Mfidi <mfidiv@gmail.com>
2025-05-16 13:12:55 +0200

    Installed & configured flask, set up virtual environment and configure SQLAlchemy

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from .db import db 
+import os
+
+def create_app():
+    #Load environment variables from a .env file
+    load_dotenv()
+
+    #Initialize the Flask app
+    app = Flask(__name__)
+
+    # Set configuration values from the environment
+    app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY') 
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Initialize SQLAlchemy with the app
+    db.init_app(app)
+
+    # Import and register the main Blueprint containing routes
+    from .routes import main
+    app.register_blueprint(main)
+
+    #Return the configured app instance
+    return app
